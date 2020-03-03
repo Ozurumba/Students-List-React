839e56208a6ad019a19e0456c6b237c5078a2cdf
Ozurumba <ozuruobinna@gmail.com>
2020-03-03 14:52:23 +0100

    first commit

---
+++ src/obinna.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Obinna(props) {
+  return (
+    <div>
+        <p>{props.name} is a good developer</p>
+        <h6>{props.address} is the name of my tutorial center</h6>
+    </div>
+  );
+}
+
+export default Obinna;
